4c272d147a7f972957f2a3a1a3fc65c30177780f
kevin <kevinz@weghst.com>
2018-03-07 16:24:34 +0800

    修改 ROOT 权限判断

--- src/main/kotlin/io/zhudy/duic/web/security/RootRoleHandler.kt
+++ src/main/kotlin/io/zhudy/duic/web/security/RootRoleHandler.kt
@@ -8,19 +8,17 @@ import reactor.core.publisher.Mono
 
 
 /**
+ * ROOT 角色处理。
+ *
  * @author Kevin Zou (kevinz@weghst.com)
  */
 class RootRoleHandler(private val next: (ServerRequest) -> Mono<ServerResponse>) : (ServerRequest) -> Mono<ServerResponse> {
 
-    /**
-     *
-     */
     override fun invoke(request: ServerRequest): Mono<ServerResponse> {
         val userContext = request.userContext()
-        if (userContext.isRoot) {
+        if (!userContext.isRoot) {
             throw BizCodeException(BizCode.Classic.C_403)
         }
         return next(request)
     }
 }
-
