c02262e29380b7de12daed12f6ec8a950d5a3d8f
kevin <kevinz@weghst.com>
2018-04-26 14:13:15 +0800

    修改 sql 脚本

--- src/main/resources/db/migration/mysql/V1.6/V1.6.0.0__add_user_table.sql
+++ src/main/resources/db/migration/mysql/V1.6/V1.6.0.0__add_user_table.sql
@@ -1,10 +1,9 @@
 CREATE TABLE `user` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR (64) NOT NULL COMMENT '登录邮箱',
-  `password` VARCHAR (256) NOT NULL COMMENT '登录密码',
-  `created_at` TIMESTAMP COMMENT '创建时间',
-  `updated_at` TIMESTAMP COMMENT '修改时间',
-  PRIMARY KEY (`id`)
-) ENGINE = INNODB CHARSET = utf8mb4 COLLATE = utf8mb4_bin;
-
-ALTER TABLE `user` ADD UNIQUE INDEX (`email`);
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(64) COLLATE utf8mb4_bin NOT NULL COMMENT '登录邮箱',
+  `password` varchar(256) COLLATE utf8mb4_bin NOT NULL COMMENT '登录密码',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT '创建时间',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
--- src/main/resources/db/migration/mysql/V1.6/V1.6.0.2__add_app_history_table.sql
+++ src/main/resources/db/migration/mysql/V1.6/V1.6.0.2__add_app_history_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `app` (
+CREATE TABLE `app_history` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) COLLATE utf8mb4_bin NOT NULL COMMENT '应用名称',
   `profile` varchar(64) COLLATE utf8mb4_bin NOT NULL COMMENT '应用环境',
@@ -7,12 +7,12 @@ CREATE TABLE `app` (
   `ip_limit` varchar(1024) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '可访问 IP',
   `v` int(11) NOT NULL COMMENT '应用版本',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT '创建时间',
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '修改时间',
   `content` text COLLATE utf8mb4_bin DEFAULT NULL COMMENT '应用配置内容',
   `users` varchar(512) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '应用所属用户',
+  `updated_by` varchar(64) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '修改者',
+  `deleted_by` varchar(64) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '删除者',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `name_profile` (`name`,`profile`),
-  KEY `created_at` (`created_at`),
-  KEY `updated_at` (`updated_at`),
-  FULLTEXT KEY `name_profile_content` (`name`,`profile`,`content`)
+  KEY `name` (`name`),
+  KEY `profile` (`profile`),
+  KEY `created_at` (`created_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
