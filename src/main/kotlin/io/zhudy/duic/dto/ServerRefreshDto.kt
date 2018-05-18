c5c0588da1640cd28c1924440815e8ac7eba9bf1
kevin <kevinz@weghst.com>
2018-05-18 17:28:04 +0800

    修改属性命名

--- src/main/kotlin/io/zhudy/duic/dto/ServerRefreshDto.kt
+++ src/main/kotlin/io/zhudy/duic/dto/ServerRefreshDto.kt
@@ -6,6 +6,6 @@ import com.fasterxml.jackson.annotation.JsonProperty
  * @author Kevin Zou (kevinz@weghst.com)
  */
 data class ServerRefreshDto(
-        @JsonProperty("last_updated_at")
-        val lastUpdatedAt: Long
+        @JsonProperty("last_data_time")
+        val lastDataTime: Long
 )
