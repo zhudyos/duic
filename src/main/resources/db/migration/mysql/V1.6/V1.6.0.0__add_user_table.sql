be55f63f357cbcc0dbbdccfdc744a311047b096a
kevin <kevinz@weghst.com>
2018-06-21 19:42:43 +0800

    删除 schema sql

--- src/main/resources/db/migration/mysql/V1.6/V1.6.0.0__add_user_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(64) COLLATE utf8mb4_bin NOT NULL COMMENT '登录邮箱',
-  `password` varchar(256) COLLATE utf8mb4_bin NOT NULL COMMENT '登录密码',
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT '创建时间',
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '修改时间',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
