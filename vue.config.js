1b12d5a82479b9f6c8f62f0248c236a661bec50e
kevin <kevinz@weghst.com>
2018-09-07 16:08:34 +0800

    引入 vue-cli

---
+++ vue.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
+
+module.exports = {
+    chainWebpack: config => {
+        config.resolve.alias
+            .set('@', resolve('src/main/web'))
+
+        config.plugin('copy')
+            .use(CopyWebpackPlugin, [[{
+                from: './node_modules/monaco-editor/min',
+                to: 'monaco-editor/min'
+            }, {
+                from: './node_modules/monaco-editor/min-maps',
+                to: 'monaco-editor/0.11.1/min-maps'
+            }, {
+                from: './src/main/web/statics/fontawesome-free-5.0.10',
+                to: 'fontawesome-free-5.0.10/'
+            }]])
+    },
+    pages: {
+        index: {
+            entry: 'src/main/web/index.js',
+            template: 'src/main/web/templates/index.html',
+            filename: 'index.html'
+        }
+    },
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:7777'
+            }
+        }
+    }
+}
