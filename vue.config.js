0264415bd5927487d1956914e8581ab937a57094
kevin <kevinz@weghst.com>
2018-09-12 17:54:23 +0800

    修改 vue 配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2018 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
@@ -6,12 +21,14 @@ function resolve(dir) {
 }
 
 module.exports = {
-    chainWebpack: config => {
-        config.resolve.alias
-            .set('@', resolve('src/main/web'))
-
-        config.plugin('copy')
-            .use(CopyWebpackPlugin, [[{
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@': resolve('src/main/web')
+            }
+        },
+        plugins: [
+            new CopyWebpackPlugin([{
                 from: './node_modules/monaco-editor/min',
                 to: 'monaco-editor/min'
             }, {
@@ -20,7 +37,8 @@ module.exports = {
             }, {
                 from: './src/main/web/statics/fontawesome-free-5.0.10',
                 to: 'fontawesome-free-5.0.10/'
-            }]])
+            }])
+        ]
     },
     pages: {
         index: {
