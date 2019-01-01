eb0c7627e24760c3d10560378a837d8be1780a1c
kevin <kevinz@weghst.com>
2019-01-01 18:06:03 +0800

    新增环境依赖检查基本代码

---
+++ src/main/kotlin/io/zhudy/duic/env/CheckEnvironmentPostProcessor.kt
@@ -0,0 +1,19 @@
+package io.zhudy.duic.env
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.env.EnvironmentPostProcessor
+import org.springframework.core.env.ConfigurableEnvironment
+
+/**
+ * 启动服务前检查依赖环境是否可用。全部检查通过用启动服务，或者返回具体的错误信息。
+ *
+ * @author Kevin Zou (kevinz@weghst.com)
+ */
+class CheckEnvironmentPostProcessor : EnvironmentPostProcessor {
+
+    override fun postProcessEnvironment(env: ConfigurableEnvironment, app: SpringApplication) {
+        // FIXME 在这里校验参数
+        println(env)
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
