e4da4dd533973ea1a83347b961a6a3065f11349d
kevin <kevinz@weghst.com>
2019-01-10 18:01:01 +0800

    移除数据源导入配置

--- src/main/kotlin/io/zhudy/duic/repository/config/RelationalDBMSConfiguration.kt
+++ src/main/kotlin/io/zhudy/duic/repository/config/RelationalDBMSConfiguration.kt
@@ -15,9 +15,11 @@
  */
 package io.zhudy.duic.repository.config
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.PlatformTransactionManager
@@ -27,7 +29,6 @@ import org.springframework.transaction.support.TransactionTemplate
  * @author Kevin Zou (kevinz@weghst.com)
  */
 @Configuration
-@ImportAutoConfiguration(DataSourceAutoConfiguration::class)
 @ConditionalOnExpression("!T(io.zhudy.duic.DBMS).forName('\${duic.dbms}').isNoSQL")
 class RelationalDBMSConfiguration {
 
