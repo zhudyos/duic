e57944c9f58c20b342d4623cdd42be454312a37f
kevin <kevinz@weghst.com>
2019-06-27 21:05:16 +0800

    新增文档注释

--- src/main/kotlin/io/zhudy/duic/annotation/NoIntegrationTest.kt
+++ src/main/kotlin/io/zhudy/duic/annotation/NoIntegrationTest.kt
@@ -1,13 +1,16 @@
 package io.zhudy.duic.annotation
 
 import org.springframework.context.annotation.Profile
-import java.lang.annotation.Documented
 
 /**
+ * [Spring Annotation Programming Model](https://github.com/spring-projects/spring-framework/wiki/spring-annotation-programming-model)
+ * 添加注解可在集群测试环境 `integration-test` 中禁用某些功能，作用范围与 [Profile] 一致。
+ *
+ * @see Profile
  * @author Kevin Zou (kevinz@weghst.com)
  */
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
 @Retention
-@Documented
+@MustBeDocumented
 @Profile("!integration-test")
 annotation class NoIntegrationTest
