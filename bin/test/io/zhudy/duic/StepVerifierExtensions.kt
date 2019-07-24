af01b99d72cfe01657becea163a51d8cd4f8d767
careyToboo <toboos@sina.com>
2019-07-24 12:51:18 +0800

    移除自动生成文件

--- bin/test/io/zhudy/duic/StepVerifierExtensions.kt
+++
@@ -1,8 +0,0 @@
-package io.zhudy.duic
-
-import reactor.test.StepVerifier
-
-/**
- * @author Kevin Zou (kevinz@weghst.com)
- */
-fun StepVerifier.LastStep.expectError(bizCode: BizCode): StepVerifier = expectErrorMatches { it is BizCodeException && it.bizCode == bizCode }
