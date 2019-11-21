74136650a68ee669863730db8fa0993948fcfd52
kevin <kevinz@weghst.com>
2019-11-21 15:07:00 +0800

    优化静态资源缓存

--- src/main/kotlin/io/zhudy/duic/web/config/WebConfig.kt
+++ src/main/kotlin/io/zhudy/duic/web/config/WebConfig.kt
@@ -21,7 +21,6 @@ import io.zhudy.kitty.spring.webflux.RestWebExceptionHandler
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.CacheControl
 import org.springframework.http.codec.ServerCodecConfigurer
 import org.springframework.http.codec.json.Jackson2JsonDecoder
 import org.springframework.http.codec.json.Jackson2JsonEncoder
@@ -32,7 +31,6 @@ import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.config.ResourceHandlerRegistry
 import org.springframework.web.reactive.config.WebFluxConfigurer
 import reactor.core.publisher.Hooks
-import java.util.concurrent.TimeUnit
 
 /**
  * @author Kevin Zou (kevinz@weghst.com)
@@ -56,15 +54,9 @@ class WebConfig(private val objectMapper: ObjectMapper) : WebFluxConfigurer {
     }
 
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-        // 单页应用，对 html 资源不进行缓存
-        registry.addResourceHandler("/*.html")
-                .addResourceLocations("classpath:/public-web-resources/")
-                .setCacheControl(CacheControl.noCache())
-
-        // 缓存 js css img 等静态资源
+        // FIXME 需要优化
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/public-web-resources/")
-                .setCacheControl(CacheControl.maxAge(7, TimeUnit.DAYS))
     }
 
     @Bean
