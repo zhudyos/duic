a5e8eedabe36280cd4bab870d629aa0a344a8982
kk <kevinz@weghst.com>
2020-03-30 19:15:01 +0800

    升级依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,22 @@
-import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
-import com.bmuschko.gradle.docker.tasks.image.Dockerfile
-import com.hierynomus.gradle.license.tasks.LicenseCheck
-import com.hierynomus.gradle.license.tasks.LicenseFormat
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+buildscript {
+    dependencies {
+        classpath("org.postgresql:postgresql:42.2.11")
+        classpath("mysql:mysql-connector-java:8.0.19")
+    }
+}
 
 plugins {
     jacoco
-    kotlin("jvm") version "1.3.41"
-    kotlin("kapt") version "1.3.41"
-    kotlin("plugin.spring") version "1.3.41"
+    kotlin("jvm") version "1.3.70"
+    kotlin("kapt") version "1.3.70"
+    kotlin("plugin.spring") version "1.3.70"
 
 //    id("io.spring.dependency-management") version "1.0.8.RELEASE"
     id("org.springframework.boot") version "2.2.0.RELEASE"
     id("com.moowork.node") version "1.3.1"
     id("com.github.hierynomus.license") version "0.15.0"
     id("com.bmuschko.docker-remote-api") version "4.10.0"
+    id("org.flywaydb.flyway") version "6.3.2"
 
     id("net.researchgate.release") version "2.8.0"
 }
@@ -33,9 +34,9 @@ configurations {
 }
 
 dependencies {
-    implementation(platform("org.springframework.boot:spring-boot-dependencies:2.2.0.RELEASE"))
-    implementation(platform("io.r2dbc:r2dbc-bom:Arabba-RELEASE"))
-    implementation(platform("com.fasterxml.jackson:jackson-bom:2.9.9"))
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:2.3.0.M3"))
+    implementation(platform("io.r2dbc:r2dbc-bom:Arabba-SR2"))
+    implementation(platform("com.fasterxml.jackson:jackson-bom:2.10.3"))
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -54,18 +55,21 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-log4j2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.mongodb:mongodb-driver-reactivestreams:1.11.0")
 
     implementation("org.valiktor:valiktor-core:0.8.0")
     implementation("com.github.ben-manes.caffeine:caffeine:2.7.0")
-    implementation("com.auth0:java-jwt:3.3.0")
     implementation("io.jsonwebtoken:jjwt-impl:0.10.7")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.10.7")
     implementation("org.simplejavamail:simple-java-mail:5.1.6")
-    implementation("io.github.java-diff-utils:java-diff-utils:4.0")
+    implementation("org.bitbucket.cowwoc:diff-match-patch:1.2")
+    implementation("com.googlecode.java-diff-utils:diffutils:1.3.0")
+
+    implementation("com.github.seancfoley:ipaddress:5.2.1")
+    implementation("org.apache.lucene:lucene-memory:8.5.0")
+
     implementation("org.springframework.security:spring-security-crypto:5.0.4.RELEASE")
 
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.data:spring-data-r2dbc:1.0.0.RELEASE")
     implementation("io.r2dbc:r2dbc-postgresql")
     implementation("dev.miku:r2dbc-mysql")
@@ -83,7 +87,7 @@ dependencies {
 }
 
 tasks {
-    withType<KotlinCompile> {
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
     }
 
@@ -91,7 +95,7 @@ tasks {
         useJUnitPlatform()
         testLogging {
             showStandardStreams = true
-            exceptionFormat = TestExceptionFormat.FULL
+            exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
         }
         failFast = true
     }
@@ -116,6 +120,23 @@ tasks {
         }
     }
 
+    task<org.flywaydb.gradle.task.FlywayMigrateTask>("migratePostgreSQL") {
+        setGroup("flyway")
+        url = project.findProperty("xim.flyway.postgres.url")?.toString() ?: "jdbc:postgresql://192.168.31.62:5432/duic"
+        user = project.findProperty("xim.flyway.postgres.user")?.toString() ?: "postgres"
+        password = project.findProperty("xim.flyway.postgres.password")?.toString() ?: "123456"
+        locations = arrayOf("filesystem:${project.projectDir.path}/db/PostgreSQL/migration")
+    }
+
+    task<org.flywaydb.gradle.task.FlywayMigrateTask>("migrateMySQL") {
+        setGroup("flyway")
+        url = project.findProperty("xim.flyway.mysql.url")?.toString() ?: "jdbc:mysql://192.168.31.61:3306"
+        user = project.findProperty("xim.flyway.mysql.user")?.toString() ?: "root"
+        password = project.findProperty("xim.flyway.mysql.password")?.toString() ?: "123456"
+        schemas = arrayOf("duic")
+        locations = arrayOf("filesystem:${project.projectDir.path}/db/MySQL/migration")
+    }
+
     license {
         headerURI = com.hierynomus.gradle.license.LicenseBasePlugin::class.java.classLoader.getResource("headers/Apache-2.0")!!.toURI()
         mapping(mapOf(
@@ -130,11 +151,11 @@ tasks {
         }
     }
 
-    withType<LicenseCheck> {
+    withType<com.hierynomus.gradle.license.tasks.LicenseCheck> {
         enabled = false
     }
 
-    task<LicenseFormat>("licenseFormatWeb") {
+    task<com.hierynomus.gradle.license.tasks.LicenseFormat>("licenseFormatWeb") {
         group = "license"
         source = fileTree(projectDir) {
             include(
@@ -156,7 +177,7 @@ tasks {
         }
     }
 
-    task<Dockerfile>("createDockerfile") {
+    task<com.bmuschko.gradle.docker.tasks.image.Dockerfile>("createDockerfile") {
         dependsOn.add("copyDockerJar")
 
         group = "docker"
@@ -180,7 +201,7 @@ tasks {
         defaultCommand("sh", "-c", "java ${'$'}JAVA_OPTS ${'$'}DUIC_OPTS -jar $jarName")
     }
 
-    task<DockerBuildImage>("buildImage") {
+    task<com.bmuschko.gradle.docker.tasks.image.DockerBuildImage>("buildImage") {
         dependsOn.add("createDockerfile")
 
         group = "docker"
@@ -197,6 +218,6 @@ tasks {
 repositories {
     mavenLocal()
     mavenCentral()
-    maven("https://jitpack.io")
     maven("https://repo.spring.io/milestone")
+    maven("https://jitpack.io")
 }
